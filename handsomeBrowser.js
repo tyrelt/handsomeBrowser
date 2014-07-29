05e62466551928ed36f299ac0dce8200db104f19
David Davidson <daviddavidson.1988@gmail.com>
2014-07-28 17:41:13 -0700

    Cleaned up the commenting

--- handsomeBrowser.js
+++ handsomeBrowser.js
@@ -6,19 +6,19 @@ function isValid(element) {
     return (element.tagName == "H1" || element.tagName == "H2" || element.tagName == "H3" || element.tagName == "H4" || element.tagName == "P" || element.tagName == "LI" || element.tagName == "BUTTON") && /\S/.test(element.innerText); // innerText, not innerHTML, to exclude things like images
 }
 
-function recurseDOM(element) {
+function recurse(element) {
     element.editedChildren = [];
     if (element.childNodes.length > 1) {
         for (var i = 0; i < element.childNodes.length; i++) {
-            if (element.childNodes[i].nodeType == 1 && recurseDOM(element.childNodes[i])) {
-                 // recurse over every child node; later, if the editedChildren array holds anything, noChildrenEdited is false
+            if (element.childNodes[i].nodeType == 1 && recurse(element.childNodes[i])) {
+                // recurse over child nodes in the process of evaluating the condition; after, if editedChildren[] holds anything, noChildrenEdited is false
                 element.editedChildren.push(element.childNodes[i]);
             }
         }
         if (element.editedChildren.length == 0 && isValid(element)) {
-             // This is a node with children that aren't edited: e.g, a <p></p> with an unedited <em></em> inside it
+            // A node with children that aren't edited: e.g, a <p></p> with an (unedited) <em></em> inside it
             bro(element);
-            return true; // Booleans passed back to the recurse() that called this one
+            return true; // The return value for the parent recurse() 
         } else {
             // If there are edited child nodes, don't edit the parent
             return false;
@@ -34,5 +34,4 @@ function recurseDOM(element) {
     }
 }
 
-var dom = document.documentElement;
-recurseDOM(dom);
+recurse(document.documentElement); // Start recursion into the DOM
