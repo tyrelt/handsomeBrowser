35a8743f3b4b8cf723826043e6eee69e55e779b2
David Davidson <daviddavidson.1988@gmail.com>
2014-08-02 15:36:11 -0700

    Added basic grunt, for linting

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+    'use strict';
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: 'localhost',
+                    livereload: true,
+                    bases: './'
+                }
+            }
+        },
+        open: {
+            all: {
+                path: 'http://localhost:<%= express.all.options.port%>'
+            }
+        },
+        watch: {
+            all: {
+                files: [
+                    'handsomeBrowser.js',
+                    'index.html',
+                    'styles.css'
+                ],
+                tasks: [
+                    'jshint'
+                    // 'htmlhint',
+                ],
+                // options: {
+                //     livereload: true
+                // }
+            }
+        },
+        jshint: {
+            files: ['handsomeBrowser.js']
+        },
+        htmlhint: {
+            build: {
+                options: {
+                    'tag-pair': true,
+                    'tagname-lowercase': true,
+                    'attr-lowercase': true,
+                    'attr-value-double-quotes': true,
+                    'doctype-first': true,
+                    'spec-char-escape': true,
+                    'id-unique': true,
+                    'head-script-disabled': true,
+                    'style-disabled': true
+                },
+                src: ['index.html']
+            }
+        }
+    });
+grunt.registerTask('default', [
+    'jshint',
+    // 'htmlhint',
+    //'express',
+    //'open', 
+    'watch'
+    ]);
+};
